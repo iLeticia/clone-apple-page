33235fe3314190fa5cf016463273d899ad20bc89
Daniel Porto <jsimpressionador@gmail.com>
2024-01-22 09:51:26 -0300

    mudei os ids de alguns elementos

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
 const nomeCorSelecionada = document.getElementById('nome-cor-selecionada');
 const tituloProduto = document.getElementById('titulo-produto');
 const imagemVisualizacao = document.getElementById('imagem-visualizacao');
-const opcaoImagem0 = document.querySelector('#imagem-0 + label > img');
-const opcaoImagem1 = document.querySelector('#imagem-1 + label > img');
-const opcaoImagem2 = document.querySelector('#imagem-2 + label > img');
+const opcaoImagem0 = document.getElementById('0-imagem-miniatura');
+const opcaoImagem1 = document.getElementById('1-imagem-miniatura');
+const opcaoImagem2 = document.getElementById('2-imagem-miniatura');
 
 const verdeCipreste = {
-  nome: 'Verde-Cipreste',
+  nome: 'Verde-cipreste',
   nomePastaImagens: 'imagens-verde-cipreste',
-  emEstoque: true,
 };
 const azulInverno = {
-  nome: 'Azul-Inverno',
+  nome: 'Azul-inverno',
   nomePastaImagens: 'imagens-azul-inverno',
-  emEstoque: true,
 };
 const meiaNoite = {
-  nome: 'Meia-Noite',
+  nome: 'Meia-noite',
   nomePastaImagens: 'imagens-meia-noite',
   emEstoque: false,
 };
 const estelar = {
   nome: 'Estelar',
   nomePastaImagens: 'imagens-estelar',
-  emEstoque: true,
 };
 const rosaClaro = {
-  nome: 'Rosa-Claro',
+  nome: 'Rosa-claro',
   nomePastaImagens: 'imagens-rosa-claro',
-  emEstoque: true,
 };
 
 const opcoesCores = [verdeCipreste, azulInverno, meiaNoite, estelar, rosaClaro];
@@ -42,7 +38,7 @@ let imagemSelecionada = 1;
 function atualizarCorSelecionada() {
   const numeroCorSelecionada = document
     .querySelector('[name="opcao-cor"]:checked')
-    .id.split('cor-')[1];
+    .id.charAt(0);
   corSelecionada = numeroCorSelecionada;
   nomeCorSelecionada.innerText = `Cor - ${opcoesCores[corSelecionada].nome}`;
   imagemVisualizacao.src = `./imagens/opcoes-cores/${opcoesCores[corSelecionada].nomePastaImagens}/imagem-${imagemSelecionada}.jpeg`;
@@ -57,7 +53,7 @@ function atualizarCorSelecionada() {
 function atualizarTamanho() {
   const opcaoTamanhoSelecionado = document
     .querySelector('[name="opcao-tamanho"]:checked')
-    .id.split('tamanho-')[1];
+    .id.charAt(0);
   tamanhoSelecionado = opcaoTamanhoSelecionado;
   if (opcoesTamanho[tamanhoSelecionado] === '41 mm') {
     imagemVisualizacao.classList.add('caixa-pequena');
@@ -75,7 +71,7 @@ function atualizarTamanho() {
 function atualizarImagemSelecionada() {
   const opcaoImagemSelecionada = document
     .querySelector('[name="opcao-imagem"]:checked')
-    .id.split('imagem-')[1];
+    .id.charAt(0);
   imagemSelecionada = opcaoImagemSelecionada;
   imagemVisualizacao.src = `./imagens/opcoes-cores/${opcoesCores[corSelecionada].nomePastaImagens}/imagem-${imagemSelecionada}.jpeg`;
 }
